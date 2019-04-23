ba85af6309dd667a78f9411c93eaf118e0422665
jfortenzi <49759417+jfortenzi@users.noreply.github.com>
2019-04-23 09:56:28 -0300

    HelloWorld.cpp
    Sample c++ program

---
+++ 00-CppHelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+/* Hello World
+*  Elección, instalación y primera prueba
+    del ambiente elegido.
+*  Alumno: Juan Francisco Ortenzi
+*  Fecha: 19-4-2019
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World!"<<std::endl;
+}
---
+++ 00-CppHelloWorld/Readme.md
@@ -0,0 +1,4 @@
+-Creación y preparación del repositorio para los trabajos de la cátedra.
+
+-Elección y preparación del ambiente de desarrollo: 
+   *Visual Studio Code
